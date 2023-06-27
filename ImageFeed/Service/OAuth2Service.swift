c555f551c5deee27e2828111e11dfdd030c48ef1
lilkxst <kastsianka.artsiom@gmail.com>
2023-06-28 01:44:44 +0300

    Добавлена обработка потенциального состояния гонки при авторизации. Добавлена блокировка UI при показе ProgressHUD

--- ImageFeed/Service/OAuth2Service.swift
+++ ImageFeed/Service/OAuth2Service.swift
@@ -18,6 +18,8 @@ final class OAuth2Service {
     
     static let shared = OAuth2Service()
     private let urlSession = URLSession.shared
+    private var task: URLSessionTask?
+    private var lastCode: String?
     private (set) var authToken: String? {
         get {
             return OAuth2TokenStorage().token
@@ -28,18 +30,36 @@ final class OAuth2Service {
     }
     
     func fetchOAuthToken(_ code: String, completion: @escaping (Result<String, Error>) -> Void) {
-        let request = authTokenRequest(code: code)
-        let task = object(for: request) { [weak self] result in
-            guard let self = self else { return }
-            switch result {
-            case .success(let body):
-                let authToken = body.accessToken
-                self.authToken = authToken
-                completion(.success(authToken))
-            case .failure(let error):
-                completion(.failure(error))
+        assert(Thread.isMainThread)
+        if task != nil {
+            if lastCode != code {
+                task?.cancel()
+            } else {
+                return
+            }
+        } else {
+            if lastCode == code {
+                return
             }
         }
+        lastCode = code
+        let request = authTokenRequest(code: code)
+        let task = object(for: request) { [weak self] result in
+            DispatchQueue.main.async {
+                guard let self = self else { return }
+                switch result {
+                case .success(let body):
+                    let authToken = body.accessToken
+                    self.authToken = authToken
+                    completion(.success(authToken))
+                    self.task = nil
+                case .failure(let error):
+                    completion(.failure(error))
+                    self.lastCode = nil
+                }
+            }
+        }
+        self.task = task
         task.resume()
     }
 }
---
+++ ImageFeed/UIBlockingProgressHUD.swift
@@ -0,0 +1,26 @@
+//
+//  UIBlockingProgressHUD.swift
+//  ImageFeed
+//
+//  Created by Артём Костянко on 28.06.23.
+//
+
+import Foundation
+import UIKit
+import ProgressHUD
+
+final class UIBlockingProgressHUD {
+    private static var window: UIWindow? {
+        return UIApplication.shared.windows.first
+    }
+    
+    static func show() {
+        window?.isUserInteractionEnabled = false
+        ProgressHUD.show()
+    }
+    
+    static func dismiss() {
+        window?.isUserInteractionEnabled = true
+        ProgressHUD.dismiss()
+    }
+}
